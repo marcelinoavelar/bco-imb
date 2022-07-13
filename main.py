745c416885f39b50ed0ca7296f6ded8b818deded
Marcelino Avelar <marcelinobg@gmail.com>
2022-07-13 00:18:28 -0300

    Add argparse and refactor GameRound

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
-import os
+import argparse
+import secrets
+
 from src.applications.use_cases.game_round import GameRound
 from src.domain.entities.property import Property
 from src.domain.entities.player import *
 
 
-if __name__ == '__main__':
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser_group = parser.add_argument_group()
+    parser_group.add_argument('-s', '--simulations', dest='simulations', type=int, required=True,
+                              help='Quantiade de simuações')
+    parser_group.add_argument('-d', '--rounds', dest='rounds',
+                              type=int, required=True,
+                              help='Quantidades de rodadas')
+    parser_group.add_argument('-x', '--min_price', dest='min_price', type=float, required=True,
+                              help='Valor minimo para geração randomica de preço das propriedades')
+    parser_group.add_argument('-p', '--max_price', dest='max_price', type=float, required=True,
+                              help='Valor maximo para geração randomica de preço das propriedades')
+    parser_group.add_argument('-r', '--min_rental', dest='min_rental', type=float, required=True,
+                              help='Valor minimo para geração randomica de aluguel das propriedades')
+    parser_group.add_argument('-t', '--max_rental', dest='max_rental', type=float, required=True,
+                              help='Valor maximo para geração randomica de aluguel das propriedades')
+    parser_group.add_argument('-c', '--game_properties', dest='game_properties', type=int, required=True,
+                              help='Quantidade de propriedades')
+    return parser.parse_args()
 
-    simulations = int(os.environ.get('SIMULATIONS', 0))
-    rounds = int(os.environ.get('ROUNDS', 0))
+
+if __name__ == '__main__':
+    args = get_parser()
+    simulations = args.simulations
+    rounds = args.rounds
+    min_price = args.min_price
+    max_price = args.max_price
+    min_rental = args.min_rental
+    max_rental = args.max_rental
+    game_properties = args.game_properties
 
     for i in range(simulations):
         properties = []
-        for i in range(20):
-            price = round(random.uniform(100_000.00, 250_000.00), 2)
-            rental = round(random.uniform(1000.00, 10_000.00), 2)
-            properties.append(Property(price=price, rental=rental))
+        for i in range(game_properties):
+            price = round(random.uniform(min_price, max_price), 2)
+            rental = round(random.uniform(min_rental, max_rental), 2)
+            property = Property(price=price, rental=rental)
+            if round(random.uniform(1, 3)) == 2:
+                property.purchase(secrets.token_urlsafe(6))
+            properties.append(property)
+
         players = [
             PlayerInpulsive("Player Inpulsive"),
             PlayerPicky("Player Picly"),
             PlayerCautious("Player Cautious"),
             PlayerRandom("Player Randon")
-
         ]
         game_round = GameRound(properties, players)
-        game_round.execute(rounds)
+        output = game_round.execute(rounds)
+        print(f'\n -> {output} ')
--- src/applications/use_cases/game_round.py
+++ src/applications/use_cases/game_round.py
@@ -6,27 +6,28 @@ from src.domain.entities.player import Player
 
 class GameRound:
 
-    def __init__(self, properties: list[Property], players: list[Player]) -> None:
+    def __init__(self, properties: list[Property], players: list[Player]) -> dict:
         self.properties = properties
         self.players = players
+        self.players_positons = self.init_positions()
 
     def execute(self, rounds: int) -> bool:
 
-        player_positons = dict({})
-        for player in self.players:
-            player_positons[player.name] = 0
-
         for i in range(rounds):
             for player in self.players:
-                plus_balance = False
-                progess = round(random.uniform(1, 6))
-                player_positons[player.name] += progess
-                if player_positons[player.name]+progess >= len(self.properties):
-                    progess = progess-len(self.properties)
-                    plus_balance = True
-                property = self.properties[progess-1:progess][0]
+                if len(self.players) == 1:
+                    output = {
+                        "rounds": i,
+                        "players": len(self.players),
+                        "winner": self.players[0].__class__.__name__,
+                        "timeout": False
+                    }
+                    return output
+                progress, plus_balance = self.get_progress(player)
+                property = self.properties[progress-1:progress][0]
                 purchase_property_service = PurchasePropertyService()
                 output = purchase_property_service.purchase(property, player)
+
                 if not output:
                     if property.owner != None:
                         player.pay_rental(property)
@@ -37,8 +38,34 @@ class GameRound:
                     if plus_balance:
                         player.add_balance(100)
 
-            if len(self.players) == 1:
-                break
+            if i+1 == rounds:
+                winner = self.game_leader()
+                output = {
+                    "rounds": i+1,
+                    "players": len(self.players),
+                    "winner": winner.__class__.__name__,
+                    "timeout": True
+                }
+                return output
 
+    def init_positions(self) -> None:
+        players_positons = dict({})
+        for player in self.players:
+            players_positons[player.name] = 0
+        return players_positons
 
-        print(f'\n Winer -> {self.players} \n')
+    def get_progress(self, player: Player) -> tuple[int, bool]:
+        plus_balance = False
+        progress = round(random.uniform(1, 6))
+        self.players_positons[player.name] += progress
+        if self.players_positons[player.name]+progress >= len(self.properties):
+            progress = progress-len(self.properties)
+            plus_balance = True
+        return progress, plus_balance
+
+    def game_leader(self) -> Player:
+        winner = self.players[0]
+        for player in self.players[1:len(self.players)]:
+            if player.balance > winner.balance:
+                winner = player
+        return winner
