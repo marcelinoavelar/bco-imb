cb1e117f25aaff21788751771c773b74722c7cd8
Marcelino Avelar <marcelinobg@gmail.com>
2022-07-13 01:08:47 -0300

    Formated display result

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import secrets
-
+import operator
+import json
+from textwrap import indent
 from src.applications.use_cases.game_round import GameRound
 from src.domain.entities.property import Property
 from src.domain.entities.player import *
@@ -10,21 +12,34 @@ def get_parser():
     parser = argparse.ArgumentParser()
     parser_group = parser.add_argument_group()
     parser_group.add_argument('-s', '--simulations', dest='simulations', type=int, required=True,
-                              help='Quantiade de simuações')
+                              help='Number of simulations')
     parser_group.add_argument('-d', '--rounds', dest='rounds',
                               type=int, required=True,
-                              help='Quantidades de rodadas')
+                              help='Number of rounds')
     parser_group.add_argument('-x', '--min_price', dest='min_price', type=float, required=True,
-                              help='Valor minimo para geração randomica de preço das propriedades')
+                              help='Minimum value for random property price generation')
     parser_group.add_argument('-p', '--max_price', dest='max_price', type=float, required=True,
-                              help='Valor maximo para geração randomica de preço das propriedades')
+                              help='Maximum value for random property price generation')
     parser_group.add_argument('-r', '--min_rental', dest='min_rental', type=float, required=True,
-                              help='Valor minimo para geração randomica de aluguel das propriedades')
+                              help='Minimum value for random generation of rental properties')
     parser_group.add_argument('-t', '--max_rental', dest='max_rental', type=float, required=True,
-                              help='Valor maximo para geração randomica de aluguel das propriedades')
+                              help='Maximum value for random generation of rental properties')
     parser_group.add_argument('-c', '--game_properties', dest='game_properties', type=int, required=True,
-                              help='Quantidade de propriedades')
+                              help='Number of properties')
     return parser.parse_args()
+
+
+def get_winners_percent_by_profile(winners: list) -> list:
+    winners_percente = dict({})
+    for winner in winners:
+        winners_percente[winner] = round(int(winners[winner])/300*100, 2)
+    return winners_percente
+
+
+def get_top_winner(winners: list) -> str:
+    winners = sorted(winners.items(), key=operator.itemgetter(1), reverse=True)
+    winners = {k: v for k, v in winners}
+    return list(winners.keys())[0]
 
 
 if __name__ == '__main__':
@@ -36,6 +51,10 @@ if __name__ == '__main__':
     min_rental = args.min_rental
     max_rental = args.max_rental
     game_properties = args.game_properties
+
+    round_end_per_timeout = 0
+    count_rounds = 0
+    winner_ranking = dict({})
 
     for i in range(simulations):
         properties = []
@@ -55,4 +74,20 @@ if __name__ == '__main__':
         ]
         game_round = GameRound(properties, players)
         output = game_round.execute(rounds)
-        print(f'\n -> {output} ')
+
+        round_end_per_timeout += int(output.get('timeout', ''))
+        count_rounds += int(output.get('rounds', ''))
+
+        try:
+            winner_ranking[output.get('winner', '')] += 1
+        except:
+            winner_ranking[output.get('winner', '')] = 1
+
+    result = {
+        "round_end_per_timeout": round_end_per_timeout,
+        "average_rounds_per_game": round(count_rounds/simulations, 2),
+        "winners_percent_by_profile": get_winners_percent_by_profile(winner_ranking),
+        "top winner": get_top_winner(winner_ranking)
+    }
+    result_json = json.dumps(result, indent=2)
+    print(result_json)
